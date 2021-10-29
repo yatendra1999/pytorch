d6b9ec90f599965480070d1c40acbffe948f0351
Philip Meier <github.pmeier@posteo.de>
2021-10-29 16:16:11 +0200

    add `OpInfo` for `torch.nn.functional.kl_div`
    ghstack-source-id: f4a13d4fb47807d1723874eeff7e6858641566a0
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/65469

--- test/onnx/test_pytorch_onnx_onnxruntime.py
+++ test/onnx/test_pytorch_onnx_onnxruntime.py
@@ -7044,16 +7044,16 @@ class TestONNXRuntime(unittest.TestCase):
     @skipIfUnsupportedMinOpsetVersion(9)
     def test_kldiv_loss(self):
 
-        x = torch.randn(5)
-        y = torch.randn(5)
+        x = torch.rand(5).log()
+        y = torch.rand(5)
         self._kldiv_loss(x, y)
 
-        x = torch.randn(2, 3, 5)
-        y = torch.randn(2, 3, 5)
+        x = torch.rand(2, 3, 5).log()
+        y = torch.rand(2, 3, 5)
         self._kldiv_loss(x, y)
 
-        x = torch.randn(2, 3, 5, 7)
-        y = torch.randn(2, 3, 5, 7)
+        x = torch.rand(2, 3, 5, 7).log()
+        y = torch.rand(2, 3, 5, 7)
         self._kldiv_loss(x, y)
 
     def _kldiv_loss(self, x, y):
@@ -7063,7 +7063,7 @@ class TestONNXRuntime(unittest.TestCase):
                 self.loss = torch.nn.KLDivLoss(reduction="none", log_target=True)
 
             def forward(self, input, target):
-                return self.loss(input, target)
+                return self.loss(input, target.log())
 
         self.run_test(KLDivLossNone(), input=(x, y))
 
@@ -7083,7 +7083,7 @@ class TestONNXRuntime(unittest.TestCase):
                 self.loss = torch.nn.KLDivLoss(reduction="sum", log_target=True)
 
             def forward(self, input, target):
-                return self.loss(input, target)
+                return self.loss(input, target.log())
 
         self.run_test(KLDivLossSum(), input=(x, y))
 
@@ -7103,7 +7103,7 @@ class TestONNXRuntime(unittest.TestCase):
                 self.loss = torch.nn.KLDivLoss(reduction="batchmean", size_average=False, log_target=True)
 
             def forward(self, input, target):
-                return self.loss(input, target)
+                return self.loss(input, target.log())
 
         self.run_test(KLDivLossMiniBatchMean(), input=(x, y))
 
