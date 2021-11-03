3244699167731b5eafa930dd821585886e7cd33b
Peter Bell <peterbell10@live.co.uk>
2021-11-03 20:52:36 +0000

    Remove native_functions.yaml dependency from TensorModeKernel.cu
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/66913
    
    Differential Revision: [D31856102](https://our.internmc.facebook.com/intern/diff/D31856102/)
    ghstack-source-id: e023c2656c4c815a71dca93dd3045f6570a5e3a6

---
+++ c10/core/DeviceArray.h
@@ -0,0 +1,21 @@
+#include <c10/core/Allocator.h>
+
+namespace c10 {
+
+template <typename T>
+class DeviceArray {
+ public:
+  DeviceArray(c10::Allocator& allocator, size_t size)
+      : data_ptr_(allocator.allocate(size * sizeof(T))) {
+    static_assert(std::is_pod<T>::value, "T must be 'plain old data'");
+  }
+
+  T* get() {
+    return static_cast<T*>(data_ptr_.get());
+  }
+
+ private:
+  c10::DataPtr data_ptr_;
+};
+
+} // namespace c10
